57276020bb20bb8c9fccac9b4876316e7ca92d82
gabolsgabs <gabolsgabs@gmail.com>
2019-11-18 19:28:17 +0100

    refactoring

--- cunet/train.py
+++ cunet/train.py
@@ -4,8 +4,6 @@ from cunet.config import config
 from cunet.models.cunet_model import cunet_model
 from cunet.models.unet_model import unet_model
 
-from tensorflow.keras.utils import plot_model
-
 logger = tf.get_logger()
 logger.setLevel(logging.INFO)
 
@@ -18,12 +16,6 @@ def main():
     if config.MODE == 'conditioned':
         model = cunet_model()
         name = "_".join([name, config.CONTROL_TYPE, config.FILM_TYPE])
-    plot_model(
-        model,
-        to_file='/Users/meseguerbrocal/Documents/PhD/code_bk_local/cunet/models/architectures/'+name+'.png',
-        show_shapes=True,
-        show_layer_names=True
-    )
     return
 
 
