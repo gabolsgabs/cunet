4e57e9de5acb6c81b763a498edd16817e615987c
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2020-02-02 18:15:33 +0100

    add ffmpeg and return audio as numpy array

--- cunet/evaluation/results_replicate.py
+++ cunet/evaluation/results_replicate.py
@@ -10,10 +10,7 @@ def separate_audio_replicate(model, path_audio):
     y, _ = analize_spec(
         spec_complex(path_audio)['spec'], model, None)
     y = (y - np.min(y))/(np.max(y) - np.min(y))
-    # name = path_audio.split('/')[-1].replace('.mp3', '.wav')
-    # name = os.path.join(path_output, name)
-    # librosa.output.write_wav(name, y, sr=config_prepro.FR)
-    return
+    return y.reshape([len(y), 1])
 
 
 def load_a_cunet():
