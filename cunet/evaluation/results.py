bug in normalization solved

cunet/evaluation/results.py
@@ -1,5 +1,6 @@
 from glob import glob
 import librosa
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 import mir_eval
 import numpy as np
@@ -167,23 +168,36 @@ def create_pandas(files):
     return df
 
 
+def load_checkpoint(path_results):
+    from cunet.train.models.unet_model import unet_model
+    from cunet.train.models.cunet_model import cunet_model
+    path_results = os.path.join(path_results, 'checkpoint')
+    latest = tf.train.latest_checkpoint(path_results)
+    if config.MODE == 'standard':
+        model = unet_model()
+    if config.MODE == 'conditioned':
+        model = cunet_model()
+    model.load_weights(latest)
+    return model
+
+
 def load_a_cunet(target=None):
     model = None
     if config.MODE == 'standard':
         path_results = os.path.join(config.PATH_MODEL, target, config.NAME)
         path_model = os.path.join(path_results, config.NAME+'.h5')
-        model = load_model(path_model)
+        if os.path.exists(path_model):
+            model = load_model(path_model)
+        else:
+            model = load_checkpoint(path_results)
     else:
-        import tensorflow as tf
         path_results = os.path.join(config.PATH_MODEL, config.NAME)
         path_model = os.path.join(path_results, config.NAME+'.h5')
         model = load_model(path_model,  custom_objects={"tf": tf})
-        # from cunet.train.config import config as config_model
-        # from cunet.train.models.cunet_model import cunet_model
-        # config_model.set_group(config.GROUP)
-        # latest = tf.train.latest_checkpoint(path_results+'/checkpoint')
-        # model = cunet_model()
-        # model.load_weights(latest)
+        if os.path.exists(path_model):
+            model = load_model(path_model,  custom_objects={"tf": tf})
+        else:
+            model = load_checkpoint(path_results)
     return model, path_results
 
 

cunet/train/data_loader.py
@@ -23,11 +23,12 @@ def get_name(txt):
     return os.path.basename(os.path.normpath(txt)).replace('.npz', '')
 
 
-def progressive(data, conditions, dx):
+def progressive(data, conditions, dx, val_set):
     output = copy.deepcopy(data)
     if (
         config.PROGRESSIVE and np.max(np.abs(data)) > 0
         and random.sample(range(0, 4), 1)[0] == 0   # 25% of doing it
+        and not val_set
     ):
         p = random.uniform(0, 1)
         conditions[dx] = conditions[dx]*p
@@ -35,7 +36,7 @@ def progressive(data, conditions, dx):
     return output[:, :, dx], conditions
 
 
-def yield_data(indexes, files):
+def yield_data(indexes, files, val_set):
     conditions = np.zeros(1).astype(np.float32)
     n_frames = config.INPUT_SHAPE[1]
     for i in indexes:
@@ -43,7 +44,7 @@ def yield_data(indexes, files):
             if len(i) > 2:
                 conditions = i[2]
             yield {'data': DATA[i[0]][:, i[1]:i[1]+n_frames, :],
-                   'conditions': conditions}
+                   'conditions': conditions, 'val': val_set}
 
 
 def load_indexes_file(val_set=False):
@@ -57,12 +58,12 @@ def load_indexes_file(val_set=False):
         # Indexes val has no overlapp in the data points
         indexes = np.load(config.INDEXES_VAL, allow_pickle=True)['indexes']
         files = VAL_FILES
-    return yield_data(indexes, files)
+    return yield_data(indexes, files, val_set)
 
 
 @tf.function(autograph=False)
 def prepare_data(data):
-    def py_prepare_data(target_complex, conditions):
+    def py_prepare_data(target_complex, conditions, val_set):
         target_complex = target_complex.numpy()
         conditions = conditions.numpy()
         if config.MODE == 'standard':
@@ -75,18 +76,18 @@ def prepare_data(data):
                 # simple conditions
                 if len(i) == 1:
                     target, conditions = progressive(
-                        target_complex, conditions, i[0])
+                        target_complex, conditions, i[0], val_set)
                 # complex conditions
                 if len(i) > 1:
                     for dx in i:
                         target_tmp, conditions = progressive(
-                            target_complex, conditions, dx)
+                            target_complex, conditions, dx, val_set)
                         target = np.sum([target, target_tmp], axis=0)
         target = np.abs(target)
         mixture = np.abs(target_complex[:, :, -1])
         return check_shape(mixture), check_shape(target), conditions
     mixture, target, conditions = tf.py_function(
-        py_prepare_data, [data['data'], data['conditions']],
+        py_prepare_data, [data['data'], data['conditions'], data['val']],
         (tf.float32, tf.float32, tf.float32)
     )
     return {'mix': mixture, 'target': target, 'conditions': conditions}
@@ -111,7 +112,7 @@ def convert_to_estimator_input(d):
 def dataset_generator(val_set=False):
     ds = tf.data.Dataset.from_generator(
         load_indexes_file,
-        {'data': tf.complex64, 'conditions': tf.float32},
+        {'data': tf.complex64, 'conditions': tf.float32, 'val': tf.bool},
         args=[val_set]
     ).map(
         prepare_data, num_parallel_calls=config.NUM_THREADS

cunet/train/load_data_offline.py
@@ -22,9 +22,21 @@ def complex_min(d):
     return d[np.unravel_index(np.argmin(np.abs(d), axis=None), d.shape)]
 
 
-def normlize_complex(data):
-    return np.divide((data - complex_min(data)),
-                     (complex_max(data) - complex_min(data)))
+def normlize_complex(data, c_max=1):
+    if c_max != 1:
+        factor = np.divide(complex_max(data), c_max)
+    else:
+        factor = 1
+    # normalize between 0-1
+    output = np.divide((data - complex_min(data)),
+                       (complex_max(data) - complex_min(data)))
+    return np.multiply(output, factor)  # scale to the original range
+
+
+def get_max_complex(data, keys):
+    # sometimes the max is not the mixture
+    pos = np.argmax([np.abs(complex_max(data[i])) for i in keys])
+    return np.array([complex_max(data[i]) for i in keys])[pos]
 
 
 def load_data(files):
@@ -39,10 +51,11 @@ def load_data(files):
             data[get_name(i)] = np.empty(
                 [*data_tmp['mix'].shape, 2], dtype=np.complex64
             )
+            c_max = get_max_complex(data_tmp, ['mix', config.TARGET])
             data[get_name(i)][:, :, 0] = normlize_complex(
-                data_tmp[config.TARGET])
+                data_tmp[config.TARGET], c_max)
             data[get_name(i)][:, :, 1] = normlize_complex(
-                data_tmp['mix'])
+                data_tmp['mix'], c_max)
         if config.MODE == 'conditioned':
             if len(sources) == 0:
                 sources = copy.deepcopy(data_tmp.files)
@@ -52,8 +65,11 @@ def load_data(files):
             data[get_name(i)] = np.empty(
                 [*data_tmp['mix'].shape, len(sources)], dtype=np.complex64
             )
+            c_max = get_max_complex(data_tmp, sources)
             for j, value in enumerate(sources):
-                data[get_name(i)][:, :, j] = normlize_complex(data_tmp[value])
+                data[get_name(i)][:, :, j] = normlize_complex(
+                    data_tmp[value], c_max
+                )
     if config.MODE == 'conditioned':
         logger.info('Source order %s' % sources)
     return data